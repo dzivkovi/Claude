0286b3fbd2fbce256436fc637369bdac090e98f3
Daniel Z. <daniel.zivkovic@nasdaq.com>
2024-11-04 15:20:22 -0500

    Minor code cleanup

--- hi_claude_bedrock.py
+++ hi_claude_bedrock.py
@@ -5,6 +5,10 @@ from anthropic import AnthropicBedrock
 
 load_dotenv()
 
+# Also try
+# "anthropic.claude-3-haiku-20240307-v1:0"
+# "anthropic.claude-3-5-sonnet-20241022-v2:0"
+ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0")
 
 def extract_response_details(llm_response: Any, client_name: str) -> tuple:
     """
@@ -30,18 +34,18 @@ def extract_response_details(llm_response: Any, client_name: str) -> tuple:
 client = AnthropicBedrock(
     # Authenticate by either providing the keys below or use the default AWS credential providers, such as
     # using ~/.aws/credentials or the "AWS_SECRET_ACCESS_KEY" and "AWS_ACCESS_KEY_ID" environment variables.
-    aws_access_key=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     # Temporary credentials can be used with aws_session_token.
     # Read more at https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_temp.html.
-    # aws_session_token=os.environ.get("AWS_SESSION_TOKEN"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
     # aws_region changes the aws region to which the request is made. By default, we read AWS_REGION,
     # and if that's not present, we default to us-east-1. Note that we do not read ~/.aws/config for the region.
     aws_region="us-east-1",
 )
 
 response = client.messages.create(
-    model="anthropic.claude-3-sonnet-20240229-v1:0",  # "anthropic.claude-3-haiku-20240307-v1:0",
+    model=ANTHROPIC_MODEL,
     max_tokens=256,
     messages=[
         {
