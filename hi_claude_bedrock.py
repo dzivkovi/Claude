6900fefad12d6162bfac258cd4771f96c1232e1a
Daniel Z. <daniel.zivkovic@nasdaq.com>
2024-11-29 09:11:00 -0500

    Linting before intergratiion into GenAI platform

--- hi_claude_bedrock.py
+++ hi_claude_bedrock.py
@@ -1,3 +1,6 @@
+"""
+Minimal example of using the Anthropic API to generate text using Claude-3 on Amazon Bedrock.
+"""
 import os
 from typing import Any
 from dotenv import load_dotenv
@@ -5,20 +8,26 @@ from anthropic import AnthropicBedrock
 
 load_dotenv()
 
+LLM_PROVIDER = os.getenv("LLM_PROVIDER", "anthropic")
+
 # Also try
 # "anthropic.claude-3-haiku-20240307-v1:0"
 # "anthropic.claude-3-5-sonnet-20241022-v2:0"
 ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0")
 
-def extract_response_details(llm_response: Any, client_name: str) -> tuple:
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+ANTHROPIC_REGION = os.getenv("ANTHROPIC_REGION", AWS_REGION)
+
+
+def extract_response_details(llm_response: Any, llm_provider: str) -> tuple:
     """
     Extracts the response details from the LLM API response object.
     """
-    if client_name == 'azure' or client_name == 'azure_openai' or client_name == 'openai':
+    if llm_provider == 'azure' or llm_provider == 'azure_openai' or llm_provider == 'openai':
         query_response = llm_response.choices[0].message.content.strip() if hasattr(llm_response, 'choices') else None
         llm_model = llm_response.model if hasattr(llm_response, 'model') else 'Unknown'
         economic_unit = llm_response.usage.total_tokens if hasattr(llm_response, 'usage') else 0
-    elif client_name == 'anthropic' or client_name == 'claude' or client_name == 'bedrock':
+    elif llm_provider == 'anthropic' or llm_provider == 'claude' or llm_provider == 'bedrock':
         query_response = llm_response.content[0].text if hasattr(llm_response, 'content') else None
         llm_model = llm_response.model if hasattr(llm_response, 'model') else 'Unknown'
         if hasattr(llm_response, 'usage'):
@@ -41,7 +50,7 @@ client = AnthropicBedrock(
     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
     # aws_region changes the aws region to which the request is made. By default, we read AWS_REGION,
     # and if that's not present, we default to us-east-1. Note that we do not read ~/.aws/config for the region.
-    aws_region="us-east-1",
+    aws_region=ANTHROPIC_REGION,
 )
 
 prompt = "Who is Prime Minister of Canada? (in one sentence)"
@@ -58,8 +67,8 @@ response = client.messages.create(
 
 # print(response)
 
-query_response, llm_model, economic_unit = extract_response_details(response, "anthropic")
+answer, model, tokens = extract_response_details(response, "bedrock")
 
-print(f"Response: {query_response}")
-print(f"Model: {llm_model}")
-print(f"Tokens: {economic_unit}")
+print(f"Response: {answer}")
+print(f"Model: {model}")
+print(f"Tokens: {tokens}")
